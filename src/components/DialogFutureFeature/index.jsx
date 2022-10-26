5def72a582b086392e193232b35d1d657335e770
DevMes11 <matisanchez11@gmail.com>
2022-10-26 16:38:23 -0300

    Feat: Coming Soon modal added

---
+++ src/components/DialogFutureFeature/index.jsx
@@ -0,0 +1,30 @@
+import ReactModal from "react-modal";
+import closeIcon from "../../assets/close.svg";
+import stylesCfg from "../../utils/modalFutureFeat.json";
+import "./styles.css";
+
+const DialogFutureFeature = (props) => {
+  const { openDialog, toggleOpenDialog } = props;
+  return (
+    <ReactModal
+      style={stylesCfg}
+      isOpen={openDialog}
+      id={"dialog-coming-soon"}
+      overlayClassName={"dialog-cooming-soon-overlay"}
+      className={"dialog-cooming-soon-content"}
+      parentSelector={() => document.querySelector("#stories-app")}
+      ariaHideApp={false}
+    >
+      <div className="close-container">
+        <img src={closeIcon} alt="X" onClick={toggleOpenDialog} />
+      </div>
+      <div className="coming-soon">
+        <h2>Coming soon</h2>
+        <p>We are working on a new way to discover and experience content. </p>
+      </div>
+      <div></div>
+    </ReactModal>
+  );
+};
+
+export default DialogFutureFeature;
--- src/components/ModalLogin/LoginIncentive/index.jsx
+++ src/components/ModalLogin/LoginIncentive/index.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
+import logoFace from "../../../assets/facebook-logo.png";
+import logoGoogle from "../../../assets/google-logo.png";
+import DialogFutureFeature from "../../DialogFutureFeature";
 import "./styles.css";
 
 const LoginIncentive = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const toggleDialogValue = () => setIsDialogOpen(!isDialogOpen);
+
   return (
     <div className="incentive-wrapper">
       <h2>Continue watching?</h2>
 
       <div className="btns-container">
-        <button className="sign-in-google">
-          <img src="" alt="Google" />
+        <button className="sign-in-google" onClick={toggleDialogValue}>
+          <img src={logoGoogle} alt="Google" />
           Sign in with Google
         </button>
-        <button className="sign-in-facebook">
-          <img src="" alt="Facebook" />
+        <button className="sign-in-facebook" onClick={toggleDialogValue}>
+          <img src={logoFace} alt="Facebook" />
           Sign in with Facebook
         </button>
         <div className="sign-in-separator">
@@ -19,13 +26,19 @@ const LoginIncentive = () => {
           <span>or</span>
           <div className="separator-line"></div>
         </div>
-        <button className="log-in">Log in</button>
+        <button className="log-in" onClick={toggleDialogValue}>
+          Log in
+        </button>
       </div>
 
       <div className="sign-container">
         <p>Don't have an account?</p>
-        <span>Sign up</span>
+        <span onClick={toggleDialogValue}>Sign up</span>
       </div>
+      <DialogFutureFeature
+        openDialog={isDialogOpen}
+        toggleOpenDialog={toggleDialogValue}
+      />
     </div>
   );
 };
